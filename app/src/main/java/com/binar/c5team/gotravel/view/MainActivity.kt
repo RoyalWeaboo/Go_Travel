7ea707c0cfb2ccdf17c1896e213d75e9acb95eb8
Malik Aziz Ali <malikazizali86@gmail.com>
2022-11-30 21:00:15 +0700

    Adding Login Feature

--- app/src/main/java/com/binar/c5team/gotravel/view/MainActivity.kt
+++ app/src/main/java/com/binar/c5team/gotravel/view/MainActivity.kt
@@ -3,7 +3,6 @@ package com.binar.c5team.gotravel.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.binar.c5team.gotravel.R
-import com.binar.c5team.gotravel.databinding.FragmentProfileBinding
 
 class MainActivity : AppCompatActivity() {
 
