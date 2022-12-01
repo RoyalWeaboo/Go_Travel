e2b654a3d6847fe798f974173642e96812e5c5b9
Faldiansyah <faldiansyahk@gmail.com>
2022-12-01 15:24:55 +0800

    Initial commit

--- app/src/main/java/com/binar/c5team/gotravel/view/RegisterFragment.kt
+++ app/src/main/java/com/binar/c5team/gotravel/view/RegisterFragment.kt
@@ -5,19 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import com.binar.c5team.gotravel.R
-import com.binar.c5team.gotravel.databinding.FragmentHomeBinding
-import com.binar.c5team.gotravel.databinding.FragmentRegisterDummyBinding
+import com.binar.c5team.gotravel.databinding.FragmentRegisterBinding
 
 class RegisterFragment : Fragment() {
-    lateinit var binding : FragmentRegisterDummyBinding
+    lateinit var binding : FragmentRegisterBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = FragmentRegisterDummyBinding.inflate(inflater, container, false)
+        binding = FragmentRegisterBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // for dropdown menu gender
+        val items = listOf("Male", "Female")
+        val adapter = ArrayAdapter(requireContext(), R.layout.list_item, items)
+        (textField.editText as? AutoCompleteTextView)?.setAdapter(adapter)
+
     }
 
 }
